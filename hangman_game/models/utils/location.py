8b2826c9b33852c133ecf70d875fc06469e4f900
Erika Assis <erika.s.assis@gmail.com>
2022-11-02 21:12:21 -0300

    update: add strip

--- hangman_game/models/utils/location.py
+++ hangman_game/models/utils/location.py
@@ -1,7 +1,7 @@
 class Localization:
     def __init__(self, local: str, region: str):
-        self.__local = local
-        self.__region = region
+        self.__local = local.strip()
+        self.__region = region.strip()
 
     @property
     def local(self) -> str:
